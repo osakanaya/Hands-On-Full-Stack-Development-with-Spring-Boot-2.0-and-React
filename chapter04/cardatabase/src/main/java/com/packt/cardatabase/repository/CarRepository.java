d5175df5e884ccd6d7664eda011a27830702426a
sashida <sashida@primagest.co.jp>
2020-11-26 01:30:43 +0900

    p.78

--- chapter04/cardatabase/src/main/java/com/packt/cardatabase/repository/CarRepository.java
+++ chapter04/cardatabase/src/main/java/com/packt/cardatabase/repository/CarRepository.java
@@ -2,33 +2,15 @@ package com.packt.cardatabase.repository;
 
 import java.util.List;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.packt.cardatabase.domain.Car;
 
+@RepositoryRestResource
 public interface CarRepository extends CrudRepository<Car, Long> {
 	
-	/// 
-	List<Car> findByBrand(String brand);
-	
-	List<Car> findByColor(String color);
-	
-	List<Car> findByYear(int year);
-	
-	///
-	List<Car> findByBrandAndModel(String brand, String model);
-
-	List<Car> findByBrandOrColor(String brand, String color);
-	
-	///
-	List<Car> findByBrandOrderByYearAsc(String brand);
-	
-	///
-	@Query("select c from Car c where c.brand = ?1")
-	List<Car> findByBrand2(String brand);
-	
-	///
-	@Query("select c from Car c where c.brand like %?1")
-	List<Car> findByBrandEndsWith(String brand);
+	List<Car> findByBrand(@Param("brand") String brand);
+	List<Car> findByColor(@Param("color") String color);
 }
