7255f0ca253becb8e73a0bcce8f3a92f9017e795
sashida <sashida@primagest.co.jp>
2020-11-28 01:48:15 +0900

    p.92

--- chapter05/cardatabase/src/main/java/com/packt/cardatabase/CardatabaseApplication.java
+++ chapter05/cardatabase/src/main/java/com/packt/cardatabase/CardatabaseApplication.java
@@ -10,6 +10,8 @@ import com.packt.cardatabase.domain.Car;
 import com.packt.cardatabase.domain.Owner;
 import com.packt.cardatabase.repository.CarRepository;
 import com.packt.cardatabase.repository.OwnerRepository;
+import com.packt.cardatabase.security.User;
+import com.packt.cardatabase.security.UserRepository;
 
 @SpringBootApplication
 public class CardatabaseApplication {
@@ -19,6 +21,9 @@ public class CardatabaseApplication {
 	
 	@Autowired
 	private OwnerRepository ownerRepository;
+	
+	@Autowired
+	private UserRepository userRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CardatabaseApplication.class, args);
@@ -36,6 +41,9 @@ public class CardatabaseApplication {
 			carRepository.save(new Car("Ford", "Mustang", "Red", "ADF-1121", 2017, 59000, owner1));
 			carRepository.save(new Car("Nissan", "Leaf", "White", "SSJ-3002", 2014, 29000, owner2));
 			carRepository.save(new Car("Toyota", "Prius", "Silver", "KKO-0212", 2018, 39000, owner2));
+
+			userRepository.save(new User("user", "$2a$04$1.YhMIgNX/8TkCKGFUONWO1waedKhQ5KrnB30fl0Q01QKqmzLf.Zi", "USER"));
+			userRepository.save(new User("admin", "$2a$04$KNLUwOWHVQZVpXyMBNc7JOzbLiBjb9Tk9bP7KNcPI12ICuvzXQQKG", "ADMIN"));
 
 		};
 	}
--- chapter05/cardatabase/src/main/java/com/packt/cardatabase/security/SecurityConfig.java
+++ chapter05/cardatabase/src/main/java/com/packt/cardatabase/security/SecurityConfig.java
@@ -1,31 +1,22 @@
 package com.packt.cardatabase.security;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-	@Bean
-	@Override
-	protected UserDetailsService userDetailsService() {
-		@SuppressWarnings("deprecation")
-		UserDetails user = User.withDefaultPasswordEncoder().username("user")
-				.password("password").roles("USER").build();
-		
-		return new InMemoryUserDetailsManager(user);
+	@Autowired
+	private UserDetailsServiceImpl userDetailsService;
+	
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
 	}
-
-//	@Override
-//	protected void configure(HttpSecurity http) throws Exception {
-//	}
 
 }
