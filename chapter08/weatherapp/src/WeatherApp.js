9a2dced299c38447626ccc538991d4b7267a016f
sashida <sashida@primagest.co.jp>
2020-12-01 20:38:46 +0900

    p.162

---
+++ chapter08/weatherapp/src/WeatherApp.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class WeatherApp extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            temp: 0,
+            desc: "",
+            icon: "",
+            loading: true
+        };
+    }
+    
+    render() {
+        const imgSrc = `http://openweathermap.org/img/w/${this.state.icon}.png`;
+
+        if (this.state.loading) {
+            return <p>Loading...</p>
+        } else {
+            return (
+                <div className="App">
+                    <p>Temperature: {this.state.temp} ℃</p>
+                    <p>Description: {this.state.description}</p>
+                    <img src={imgSrc} alt="Weather icon" />
+                </div>
+            )
+        }
+    }
+
+    componentDidMount() {
+        fetch('http://api.openweathermap.org/data/2.5/weather?q=London&units=Metric&appid=6f3e935c7e409bc576abeb9dc35ae3d0')
+        .then(response => response.json())
+        .then(responseData => {
+            this.setState({
+                temp: responseData.main.temp,
+                desc: responseData.weather[0].description,
+                icon: responseData.weather[0].icon,
+                loading: false
+            })
+        })
+        .catch(err => console.error(err));
+    }
+}
+
+export default WeatherApp;
