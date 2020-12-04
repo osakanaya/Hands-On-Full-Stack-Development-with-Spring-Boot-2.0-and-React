6e43b701ea387c452677ac7fe67fdc1534c9e929
sashida <sashida@primagest.co.jp>
2020-12-04 22:37:21 +0900

    p.239

--- chapter10/carfront/src/App.test.js
+++ chapter10/carfront/src/App.test.js
@@ -1,8 +1,15 @@
-import { render, screen } from '@testing-library/react';
+import React from "react";
+import ReactDOM from "react-dom";
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import { calcMulti } from "./multi";
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+test("2 * 3 equals 6", () => {
+  expect(calcMulti(2, 3)).toBe(6);
 });
