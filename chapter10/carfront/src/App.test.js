585d5e12e7673160bceec33107ae6a5d7a1fbc3b
sashida <sashida@primagest.co.jp>
2020-12-04 22:46:37 +0900

    p.243

--- chapter10/carfront/src/App.test.js
+++ chapter10/carfront/src/App.test.js
@@ -4,6 +4,9 @@ import App from './App';
 
 import { calcMulti } from "./multi";
 
+import TestRenderer from "react-test-renderer";
+import AddCar from "./components/AddCar";
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
@@ -12,4 +15,9 @@ it('renders without crashing', () => {
 
 test("2 * 3 equals 6", () => {
   expect(calcMulti(2, 3)).toBe(6);
+});
+
+it('renders a snapshot', () => {
+  const tree = TestRenderer.create(<AddCar />).toJSON();
+  expect(tree).toMatchSnapshot();
 });
